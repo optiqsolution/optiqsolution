21e1d748b2a9e8f18c4b4a188b69b4e120bc0159
Viraj Lakshitha <viraj@getrevscale.com>
2024-11-24 16:10:54 -0500

    feat: integrate calcom scheduling feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Process from './components/Process';
-import Services from './components/Services';
-import Technologies from './components/Technologies';
+import { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Process from "./components/Process";
+import Services from "./components/Services";
+import Technologies from "./components/Technologies";
 
 function App() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({ namespace: "30min" });
+      cal("floatingButton", {
+        calLink: "optiq/15min",
+        config: { layout: "month_view", theme: "light" },
+        buttonText: "Quick chat?",
+        buttonColor: "#2564eb",
+      });
+      cal("ui", {
+        theme: "light",
+        styles: { branding: { brandColor: "#000000" } },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    })();
+  }, []);
+
   return (
     <div className="min-h-screen">
       <Header />
